042e43550dcdc1fa5bd8a078cb93a0ba86de7171
Abhishek Kanungo_AC <abhishek.kanungo.cmaug26@gmail.com>
2026-08-18 17:48:57 +0530

    Delete Assignments/Logic Building/LB_Ass_Solutions directory

--- Assignments/Logic Building/LB_Ass_Solutions/LeapYear.java
+++
@@ -1,9 +0,0 @@
-public class LeapYear {
-    public static void main(String[] args) {
-        int y = 1900;
-        if ((y%4 == 0 && y%100 != 0) || y%400 == 0) {
-            System.out.println(y+" is leap year");
-        }else{ System.out.println(y+" not a leap year");
-        }
-    }
-}
