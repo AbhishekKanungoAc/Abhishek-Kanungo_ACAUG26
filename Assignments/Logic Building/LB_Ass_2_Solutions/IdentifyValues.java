4c6e6bae436d3ee3668c9b00082ce4a5ec016d39
Abhishek Kanungo_AC <abhishek.kanungo.cmaug26@gmail.com>
2026-08-18 17:56:52 +0530

    Add files via upload

---
+++ Assignments/Logic Building/LB_Ass_2_Solutions/IdentifyValues.java
@@ -0,0 +1,20 @@
+public class IdentifyValues {
+    public static void main(String[] args) {
+        byte a; 
+        short b;
+        int c;  
+        long d; 
+        float e; 
+        double f;
+        char g; 
+        boolean h;
+        System.out.println(a);  //variable h might not have been initialized
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+         System.out.println(h);  //variable h might not have been initialized
+    }
+    
+}
